2512d71d20c7ca8c3b74e25b16f4dff380328175
lijiankou <lijiankou@bigo.sg>
2020-10-03 20:51:53 +0800

    update

--- text_classification/train.py
+++ text_classification/train.py
@@ -1,4 +1,9 @@
 import os
+import sys
+sys.path.insert(0, '../tool')
+import log
+
+import os
 import logging
 import argparse
 
@@ -10,6 +15,44 @@ from torch.utils.data import DataLoader
 
 from model import TextSentiment
 from torch.utils.data.dataset import random_split
+
+def ArgParse():
+    parser = argparse.ArgumentParser(
+        description='Train a text classification model on text classification datasets.')
+    parser.add_argument('dataset', choices=text_classification.DATASETS)
+    parser.add_argument('--num-epochs', type=int, default=5,
+                        help='num epochs (default=5)')
+    parser.add_argument('--embed-dim', type=int, default=32,
+                        help='embed dim. (default=32)')
+    parser.add_argument('--batch-size', type=int, default=16,
+                        help='batch size (default=16)')
+    parser.add_argument('--split-ratio', type=float, default=0.95,
+                        help='train/valid split ratio (default=0.95)')
+    parser.add_argument('--lr', type=float, default=4.0,
+                        help='learning rate (default=4.0)')
+    parser.add_argument('--lr-gamma', type=float, default=0.8,
+                        help='gamma value for lr (default=0.8)')
+    parser.add_argument('--ngrams', type=int, default=2,
+                        help='ngrams (default=2)')
+    parser.add_argument('--num-workers', type=int, default=1,
+                        help='num of workers (default=1)')
+    parser.add_argument('--device', default='cpu',
+                        help='device (default=cpu)')
+    parser.add_argument('--data', default='.data',
+                        help='data directory (default=.data)')
+    parser.add_argument('--use-sp-tokenizer', type=bool, default=False,
+                        help='use sentencepiece tokenizer (default=False)')
+    parser.add_argument('--sp-vocab-size', type=int, default=20000,
+                        help='vocab size in sentencepiece model (default=20000)')
+    parser.add_argument('--dictionary',
+                        help='path to save vocab')
+    parser.add_argument('--save-model-path',
+                        help='path for saving model')
+    parser.add_argument('--logging-level', default='WARNING',
+                        help='logging level (default=WARNING)')
+    args = parser.parse_args()
+    return args
+
 
 r"""
 This file shows the training process of the text classification model.
@@ -50,27 +93,15 @@ and text() functions.
 
 """
 
-
-def train_and_valid(lr_, sub_train_, sub_valid_, model = '', loss_fun = ''):
-    r"""
-    We use a SGD optimizer to train the model here and the learning rate
-    decreases linearly with the progress of the training process.
-
-    Arguments:
-        lr_: learning rate
-        sub_train_: the data used to train the model
-        sub_valid_: the data used for validation
-    """
-
+def TrainValid(num_epochs, num_workers, device, batch_size,
+               lr_, lr_gamma, train, valid, model = ''):
     optimizer = torch.optim.SGD(model.parameters(), lr=lr_)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=args.lr_gamma)
-    train_data = DataLoader(sub_train_, batch_size=batch_size, shuffle=True,
-                            collate_fn=generate_batch, num_workers=args.num_workers)
-    num_lines = num_epochs * len(train_data)
-
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=lr_gamma)
+    train_data = DataLoader(train, batch_size=batch_size, shuffle=True,
+                            collate_fn=generate_batch, num_workers=num_workers)
+    num_lines = num_epochs * len(train)
+    loss_fun = torch.nn.CrossEntropyLoss().to(device)
     for epoch in range(num_epochs):
-
-        # Train the model
         for i, (text, offsets, cls) in enumerate(train_data):
             optimizer.zero_grad()
             text, offsets, cls = text.to(device), offsets.to(device), cls.to(device)
@@ -84,30 +115,19 @@ def train_and_valid(lr_, sub_train_, sub_valid_, model = '', loss_fun = ''):
                 sys.stderr.write(
                     "\rProgress: {:3.0f}% lr: {:3.3f} loss: {:3.3f}".format(
                         progress * 100, scheduler.get_lr()[0], loss))
-        # Adjust the learning rate
         scheduler.step()
-
-        # Test the model on valid set
-        print("")
         print("Valid - Accuracy: {}".format(test(sub_valid_, model)))
 
-
-def test(data_, model = ''):
-    r"""
-    Arguments:
-        data_: the data used to train the model
-    """
+def test(batch_size, data_, model = ''):
     data = DataLoader(data_, batch_size=batch_size, collate_fn=generate_batch)
     total_accuracy = []
     for text, offsets, cls in data:
         text, offsets, cls = text.to(device), offsets.to(device), cls.to(device)
         with torch.no_grad():
             output = model(text, offsets)
-            print(output.argmax(1) == cls).float().mean()
             accuracy = (output.argmax(1) == cls).float().mean().item()
             total_accuracy.append(accuracy)
 
-    # In case that nothing in the dataset
     if total_accuracy == []:
         return 0.0
 
@@ -115,87 +135,34 @@ def test(data_, model = ''):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='Train a text classification model on text classification datasets.')
-    parser.add_argument('dataset', choices=text_classification.DATASETS)
-    parser.add_argument('--num-epochs', type=int, default=5,
-                        help='num epochs (default=5)')
-    parser.add_argument('--embed-dim', type=int, default=32,
-                        help='embed dim. (default=32)')
-    parser.add_argument('--batch-size', type=int, default=16,
-                        help='batch size (default=16)')
-    parser.add_argument('--split-ratio', type=float, default=0.95,
-                        help='train/valid split ratio (default=0.95)')
-    parser.add_argument('--lr', type=float, default=4.0,
-                        help='learning rate (default=4.0)')
-    parser.add_argument('--lr-gamma', type=float, default=0.8,
-                        help='gamma value for lr (default=0.8)')
-    parser.add_argument('--ngrams', type=int, default=2,
-                        help='ngrams (default=2)')
-    parser.add_argument('--num-workers', type=int, default=1,
-                        help='num of workers (default=1)')
-    parser.add_argument('--device', default='cpu',
-                        help='device (default=cpu)')
-    parser.add_argument('--data', default='.data',
-                        help='data directory (default=.data)')
-    parser.add_argument('--use-sp-tokenizer', type=bool, default=False,
-                        help='use sentencepiece tokenizer (default=False)')
-    parser.add_argument('--sp-vocab-size', type=int, default=20000,
-                        help='vocab size in sentencepiece model (default=20000)')
-    parser.add_argument('--dictionary',
-                        help='path to save vocab')
-    parser.add_argument('--save-model-path',
-                        help='path for saving model')
-    parser.add_argument('--logging-level', default='WARNING',
-                        help='logging level (default=WARNING)')
-    args = parser.parse_args()
+    logger = log.GetLogger(log.logging.INFO)
+    a = ArgParse()
 
-    num_epochs = args.num_epochs
-    embed_dim = args.embed_dim
-    batch_size = args.batch_size
-    lr = args.lr
-    device = args.device
-    data = args.data
-    split_ratio = args.split_ratio
-    # two args for sentencepiece tokenizer
-    use_sp_tokenizer = args.use_sp_tokenizer
-    sp_vocab_size = args.sp_vocab_size
-criterion
-    logging.basicConfig(level=getattr(logging, args.logging_level))
+    logging.basicConfig(level=getattr(logging, a.logging_level))
 
-    if not os.path.exists(data):
-        print("Creating directory {}".format(data))
+    if not os.path.exists(a.data):
+        print("Creating directory {}".format(a.data))
         os.mkdir(data)
 
-    if use_sp_tokenizer:
-        import spm_dataset
-        train_dataset, test_dataset = spm_dataset.setup_datasets(args.dataset,
-                                                                 root='.data',
-                                                                 vocab_size=sp_vocab_size)
-        model = TextSentiment(sp_vocab_size, embed_dim,
-                              len(train_dataset.get_labels())).to(device)
+    logger.info(a.dataset)
+    logger.info(a.data)
+    train_data, test_data = text_classification.DATASETS[a.dataset](root=a.data, ngrams=a.ngrams)
+    model = TextSentiment(len(train_data.get_vocab()),
+                              a.embed_dim, len(train_data.get_labels())).to(a.device)
 
-    else:
-        train_dataset, test_dataset = text_classification.DATASETS[args.dataset](
-            root=data, ngrams=args.ngrams)
-        model = TextSentiment(len(train_dataset.get_vocab()),
-                              embed_dim, len(train_dataset.get_labels())).to(device)
+    train_len = int(len(train_data) * a.split_ratio)
+    train, valid = random_split(train_data, [train_len, len(train_data) - train_len])
 
-    loss_fun = torch.nn.CrossEntropyLoss().to(device)
 
-    # split train_dataset into train and valid
-    train_len = int(len(train_dataset) * split_ratio)
-    sub_train_, sub_valid_ = \
-        random_split(train_dataset, [train_len, len(train_dataset) - train_len])
-
-    train_and_valid(lr, sub_train_, sub_valid_, model = model, loss_fun = loss_fun)
-    acc = test(test_dataset, model)
+    TrainValid(a.num_epochs, a.num_worders, a.device, a.batch_size, a.lr, a.lr_gamma,
+               train, valid, model = model)
+    acc = test(test_data, model)
     print("Test - Accuracy: {}".format(acc))
 
-    if args.save_model_path:
-        print("Saving model to {}".format(args.save_model_path))
-        torch.save(model.to('cpu'), args.save_model_path)
+    if a.save_model_path:
+        logger.info(a.save_model_path)
+        torch.save(model.to('cpu'), a.save_model_path)
 
-    if args.dictionary is not None:
-        print("Save vocab to {}".format(args.dictionary))
-        torch.save(train_dataset.get_vocab(), args.dictionary)
+    if a.dictionary is not None:
+        print("Save vocab to {}".format(a.dictionary))
+        torch.save(train_dataset.get_vocab(), a.dictionary)
